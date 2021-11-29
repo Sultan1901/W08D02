c77173d6ac62bcbf1b29008ef97cade2db739124
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-11-29 19:54:03 +0300

    add bcrypt and jsonwebtoken

--- routes/contoller/user.js
+++ routes/contoller/user.js
@@ -2,5 +2,23 @@ const userModel = require('./../../db/models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const register = async(req,res)=>{
-    const {email, password , role}
+    const {email, password , role} = req.body
+    const SALT = Number(process.env.SALT)
+    const semail = email.toLowerCase()
+    const hashpass = await bcrypt.hash(password,SALT)
+    const newUser = new userModel({
+        email : semail,
+        password : hashpass,
+        role
+    })
+    newUser
+    .save()
+    .then((result)=>{
+        res.status(201).json(result)
+
+    })
+    .catch((err)=>{
+        res.status(400).json(err)
+    })
 }
+module.exports = register
